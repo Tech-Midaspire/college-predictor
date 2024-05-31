784ce574857018338cfec51d1c2c67853a3e260b
RITIK <ritikkumar23aug2002@gmail.com>
2024-05-31 16:32:56 +0530

    button hover effect

--- components/Contact.js
+++ components/Contact.js
@@ -75,7 +75,7 @@ const ContactForm = ({ onFormSubmit }) => {
             </div>
             <div className="flex items-center justify-between">
                 <button
-                    className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    className="bg-blue-500 hover:bg-red-700  text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                     type="submit"
                 >
                     Submit
