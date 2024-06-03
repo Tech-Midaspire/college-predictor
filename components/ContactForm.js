68712c41b26325eb5924c1679ae52479b3c66886
RITIK <ritikkumar23aug2002@gmail.com>
2024-06-03 17:06:05 +0530

    Added loader

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -21,13 +21,11 @@ const ContactForm = ({ onFormSubmit }) => {
         setError(null);
 
         if (formData.name && formData.email && formData.contact) {
-
             const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
             if (!emailRegex.test(formData.email)) {
                 setError('Invalid email format');
                 return;
             }
-
 
             const contactRegex = /^[0-9]{10}$/;
             if (!contactRegex.test(formData.contact)) {
@@ -63,7 +61,7 @@ const ContactForm = ({ onFormSubmit }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="bg-black rounded px-8 pt-6 pb-8 mb-4">
+        <form onSubmit={handleSubmit} className="bg-black text-white rounded px-8 pt-6 pb-8 mb-4">
             {error && <div className="mb-4 text-red-500">{error}</div>}
             <div className="mb-4">
                 <label className="block text-white text-sm font-bold mb-2" htmlFor="name">
@@ -110,9 +108,9 @@ const ContactForm = ({ onFormSubmit }) => {
                     required
                 />
             </div>
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-center lg:mt-4">
                 <button
-                    className="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    className="bg-blue-800 hover:bg-red-700 text-white  font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                     type="submit"
                 >
                     Submit
@@ -122,5 +120,4 @@ const ContactForm = ({ onFormSubmit }) => {
     );
 };
 
-
-export default ContactForm
+export default ContactForm;
