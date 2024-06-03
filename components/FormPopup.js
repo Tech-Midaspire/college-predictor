4b0a067f2d6a75bd97a224ce382136cd8cbec756
RITIK <ritikkumar23aug2002@gmail.com>
2024-06-03 22:17:29 +0530

    Removing the local storage

--- components/FormPopup.js
+++ components/FormPopup.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import ContactForm from './ContactForm';
 
 const FormPopup = ({ children }) => {
@@ -6,20 +6,12 @@ const FormPopup = ({ children }) => {
     const [formSubmitted, setFormSubmitted] = useState(false);
     const [isSubmitting, setIsSubmitting] = useState(false);
 
-    useEffect(() => {
-        const isFormSubmitted = localStorage.getItem('formSubmitted');
-        if (isFormSubmitted) {
-            setShowPopup(false);
-            setFormSubmitted(true);
-        }
-    }, []);
-
     const handleFormSubmit = async () => {
         setIsSubmitting(true);
         try {
+            // Simulate form submission delay
             await new Promise(resolve => setTimeout(resolve, 500));
             setFormSubmitted(true);
-            localStorage.setItem('formSubmitted', 'true');
             setShowPopup(false);
         } catch (error) {
             console.error("Form submission failed", error);
@@ -33,16 +25,17 @@ const FormPopup = ({ children }) => {
             {showPopup && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
                     <div className="bg-black p-8 rounded-lg relative shadow-lg text-white">
-                        {isSubmitting && (
+                        {isSubmitting ? (
                             <div className="flex justify-center items-center">
                                 <div className="w-20 h-20 border-t-4 border-b-4 border-white rounded-full animate-spin"></div>
                             </div>
+                        ) : (
+                            <ContactForm onFormSubmit={handleFormSubmit} />
                         )}
-                        {!isSubmitting && <ContactForm onFormSubmit={handleFormSubmit} />}
                     </div>
                 </div>
             )}
-            {formSubmitted ? children : null}
+            {formSubmitted && children}
         </>
     );
 };
