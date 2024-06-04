35f7718ba0b1dcef806a305c5b8eab1171168645
RITIK <ritikkumar23aug2002@gmail.com>
2024-06-04 16:11:14 +0530

    added domain

--- pages/api/submit-form.js
+++ pages/api/submit-form.js
@@ -3,7 +3,7 @@ import dbConnect from '../../lib/mongodb';
 import FormSubmission from '../../models/formSubmission';
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', ' https://college-predictor-beta.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://eduaspire.online/');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
