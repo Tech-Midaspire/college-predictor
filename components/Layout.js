7db04d3ae0bb9fd04edce423548f8a95f3e19d4a
RITIK <ritikkumar23aug2002@gmail.com>
2024-06-11 12:27:41 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,10 @@ import FormPopup from "./FormPopup";
 const Layout = ({ children }) => {
   return (
     <>
+     <Head>
+        <link rel="icon" href="/favicon-3.png" /> {/* Link to your favicon */}
+        <title>Eduaspire</title>
+      </Head>
     <FormPopup/>
       <Navbar item1="College Predictor" item2="Scholarships" />
       <main>{children}</main>
