f6b673aa9fd1ea462d6af0f26df7ae0148c837de
RITIK <ritikkumar23aug2002@gmail.com>
2024-06-11 12:32:26 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,21 @@
 import React from "react";
 import Navbar from "./navbar";
 import FormPopup from "./FormPopup";
-
+import Head from "next/head";
 const Layout = ({ children }) => {
   return (
-    <>
-     <Head>
+    <div className="bg-slate-950">
+      
+        
+      <Head>
         <link rel="icon" href="/favicon-3.png" /> {/* Link to your favicon */}
         <title>Eduaspire</title>
       </Head>
-    <FormPopup/>
+      
+      <FormPopup />
       <Navbar item1="College Predictor" item2="Scholarships" />
       <main>{children}</main>
-    </>
+    </div>
   );
 };
 
